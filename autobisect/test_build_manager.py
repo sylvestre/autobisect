41f3d50dada405bac9217d9418a575b6a7f531a0
Tyson Smith <tyson.w.smith@gmail.com>
2017-11-20 15:17:25 -0800

    [tests] Add simple test for get_build()

--- autobisect/test_build_manager.py
+++ autobisect/test_build_manager.py
@@ -1,7 +1,5 @@
-import configparser
 import os
 import shutil
-import sqlite3
 import sys
 import tempfile
 import unittest
@@ -26,6 +24,16 @@ def _create_test_config(fname, storage_path):
         out_fp.write('storage-path: %s\n' % storage_path)
         out_fp.write('persist: true\n')
         out_fp.write('persist-limit: 30000\n')
+
+class DummyBuild(object):
+    def __init__(self, changeset, size=1):
+        self.changeset = changeset
+        self.size = size
+
+    def extract_build(self, target_path):
+        os.mkdir(target_path)
+        with open(os.path.join(target_path, "test_bin"), "w") as out_fp:
+            out_fp.write("a" * self.size)
 
 
 class DatabaseManagerTests(TestCase):
@@ -57,7 +65,7 @@ class DatabaseManagerTests(TestCase):
 
 
 class BisectionConfigTests(TestCase):
-    
+
     def setUp(self):
         self.tmpdir = tempfile.mkdtemp(prefix='bc_test_')
 
@@ -99,6 +107,35 @@ class BuildManagerTests(TestCase):
         self.assertEqual(bm.build_prefix, build_string)
         self.assertTrue(os.path.isdir(bm.build_dir))
         self.assertEqual(bm.current_build_size, 0)
-        self.assertFalse(bm.enumerate_builds())
+        self.assertEqual(len(bm.enumerate_builds()), 0)
         bm.remove_old_builds()
 
+    def test_02(self):
+        'test get_build()'
+        bm = BuildManager(self.test_ini, "foo")
+        bm.config.persist_limit = 1  # to test removing old builds
+        # grab the first build
+        commit_1 = "change123"
+        build_1 = DummyBuild(commit_1, size=1)
+        with bm.get_build(build_1) as _:
+            pass
+        self.assertEqual(bm.current_build_size, 1)
+        builds = bm.enumerate_builds()
+        self.assertEqual(len(builds), 1)
+        self.assertEqual('-'.join([bm.build_prefix, commit_1]), os.path.basename(builds[0].path))
+        # grab the second build
+        commit_2 = "change234"
+        build_2 = DummyBuild(commit_2, size=1)
+        with bm.get_build(build_2) as _:
+            pass
+        self.assertEqual(bm.current_build_size, 2)
+        self.assertEqual(len(bm.enumerate_builds()), 2)
+        # grab the third build
+        commit_3 = "change345"
+        build_3 = DummyBuild(commit_3, size=1)
+        with bm.get_build(build_3) as _:
+            pass
+        # currently the check in remove_old_builds() only runs once the size limit is exceeded
+        # and it removes builds until the current total size of the builds is less than the limit
+        self.assertEqual(bm.current_build_size, 1)
+        self.assertEqual(len(bm.enumerate_builds()), 1)
